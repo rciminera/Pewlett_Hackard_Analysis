a3124e44b9f4c7c900e331d606882985cd0de3ac
Bob Ciminera <bobc@Bobs-MBP.fios-router.home>
2021-04-01 10:43:59 -0400

    challenge queries and csv upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,3 +1,4 @@
+-- Challenge Code Starts Here
 -- Create a retirement_titles table with titles for employees born between 1952 and 1955
 SELECT e.emp_no, e.first_name, e.last_name,
 					t.title, t.from_date, t.to_date
@@ -23,3 +24,17 @@ INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY count DESC;
+
+-- Create mentorship_eligibility.csv table 
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date,
+					de.from_date, de.to_date, t.title
+INTO mentorship_elibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ 	AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
+
